b9ed25d93b909c57fc025806234cb5bd2f042359
Richard Falk-Wallace <rwf2110@gmail.com>
2021-10-24 20:19:05 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solutions in this file
+
+/*for (let age=30; age<40;age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);  
+}
+*/
+
+/*
+const gifts = ["teddy bear","drone","doll"];
+
+
+function wrapGifts(gifts){
+    for (let i = 0;i<gifts.length;i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+}
+
+wrapGifts(gifts)
+
+
+
+function writeCards(namesList,eventName){
+    cardsList = []
+    for (let counter = 0; counter < namesList.length; counter++){
+        cardsList.push(`Thank you, ${namesList[counter]}, for the wonderful birthday gift!`);
+        
+
+    }
+
+    return cardsList
+
+}
+
+namesList = ["Liam","Teddy","Jake","Toby","Hugo","Aaron"]
+
+console.log(writeCards(namesList))
+*/
+
+function countDown(number){
+    for (let i = number; i>=0;i--){
+        console.log(i);
+    }
+}
+
+countDown(10)
+
+
+
+
+
+
